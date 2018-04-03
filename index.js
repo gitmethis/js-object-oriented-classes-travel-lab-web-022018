5e8e6ae45588c5c13030d0e79ecacae3f9059c7a
Chase <chaseit12@gmail.com>
2018-04-03 22:00:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ class Driver{
   }
   
  yearsExperienceFromBeginningof(year){
-   
+    let endDate = new Date(year, 1, 1)
+    let totalYears = (endDate - this.startDate)/(365*24*60*60*1000)
+    return parseInt(totalYears)
  }
 }
 
